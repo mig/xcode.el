e0846cd15b5799481777c522dd65fd1b793db5d5
mig <matt.swasey@gmail.com>
2011-06-11 17:27:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+xcode.el
+========
+
+This library attempts to ease the development of Xcode projects within Emacs.
+
+Currently, you can do two things:
+  1. Compile (build) the project
+  2. Compile (build) and run the project
+  
+Installation
+============
+
+If you are using [el-get](https://github.com/dimitri/el-get), use the following recipe:
+
+    (:name xcode
+       :type git
+       :url "https://github.com/mig/xcode.el.git"
+       :load "xcode.el")
+
+Traditional way:
+
+    $ cd ~/.emacs.d/anywhere-you-want
+    $ git clone git://github.com/mig/xcode.el
+    
+In your emacs config:
+
+    (load "anywhere-you-want/xcode.el/xcode.el")
+    
+Usage
+=====
+
+This library provides two interactive functions:
+  
+    (xcode-compile)
+    
+and...
+
+    (xcode-compile-and-run)
+
+So you could bind those to keys for whatever mode you want. I've been having fun with MacRuby so I did something like this:
+
+    (define-key ruby-mode-map (kbd "M-r b") 'xcode-compile)
+    (define-key ruby-mode-map (kbd "M-r r") 'xcode-compile-and-run)
+    
+Notes
+=====
+
+At the moment I've only tested this library in the development version of Cocoa Emacs (24)
+
---
+++ xcode.el
@@ -0,0 +1,44 @@
+(defun buffer-dir ()
+  (file-name-directory buffer-file-name))
+
+(defun parent-dir (dir)
+  (expand-file-name (concat (file-name-as-directory dir) "..")))
+
+(defun home-dir ()
+  (expand-file-name "~/"))
+
+(defun home-dir-p (dir)
+  (equal dir (home-dir)))
+
+(defun file-for-pattern-in-dir (pattern dir)
+  (let ((match (directory-files dir nil pattern)))
+    (when match
+      (pop match))))
+
+(defun dir-of-file-found-walking-up-fs (pattern &optional current-directory)
+  (let (found-file)
+    (when (null current-directory) (setq current-directory (buffer-dir)))
+    (cond ((file-for-pattern-in-dir pattern current-directory) (expand-file-name current-directory))
+          ((home-dir-p current-directory) nil)
+          (t (dir-with-of-found-walking-up-fs pattern (parent-dir current-directory))))))
+
+(defun xcode-project-dir ()
+  (dir-with-file-found-walking-up-fs "xcodeproj"))
+
+(defun xcode-project-debug-dir ()
+  (expand-file-name (concat (xcode-project-dir) "/build/Debug/")))
+
+(defun xcode-compile ()
+  (interactive)
+    (let (working-dir)
+      (setq working-dir (xcode-project-dir))
+      (cat-run (list "cd" working-dir "&&" "xcodebuild" "-configuration" "Debug"))))
+
+(defun xcode-compile-and-run ()
+  (interactive)
+  (xcode-compile)
+  (let (app cmd)
+    (setq app (file-for-pattern-in-dir "app" (xcode-project-debug-dir)))
+    (setq cmd (combine-and-quote-strings (list "open" (concat (xcode-project-debug-dir) app))))
+    (when app
+        (call-process-shell-command cmd))))
